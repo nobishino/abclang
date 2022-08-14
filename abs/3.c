01625302a367e33e4132146cd856bb05d031c4a1
nobishino <nobishii11@gmail.com>
2022-08-14 21:31:13 +0900

    ABC081A Placing Marbles

---
+++ abs/3.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    char s[4];
+    scanf("%s", s);
+    int ans = 0;
+    for (int i = 0; i < 3; i++)
+    {
+        if (s[i] == '1')
+            ans++;
+    }
+    printf("%d\n", ans);
+}
