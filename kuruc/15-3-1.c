67e6c4d742002499b695f5ec8e4b7589faf74e33
nobishino <nobishii11@gmail.com>
2022-08-21 20:01:35 +0900

    Fixup: Chapter 15 kuruc

--- kuruc/15-3-1.c
+++ kuruc/15-3-1.c
@@ -1,34 +1,39 @@
 #include <stdio.h>
 
-int max(int xs[]);
+void calc(int xs[], int *max, int *min);
 
 int main(void)
 {
     int inputs[10];
+    int max, min;
     for (int i = 0; i < 10; i++)
     {
         scanf("%d", &inputs[i]);
         if (inputs[i] == -1)
             break;
     }
-    int ans;
-    ans = max(inputs);
 
-    printf("max value = %d\n", ans);
+    calc(inputs, &max, &min);
+
+    printf("max value = %d\n", max);
+    printf("min value = %d\n", min);
 
     return 0;
 }
 
-int max(int xs[])
+void calc(int xs[], int *max, int *min)
 {
-    int ans = 0;
+    *max = -1;
+    *min = 10000;
     for (int i = 0; i < 10; i++)
     {
         int x = xs[i];
         if (x == -1)
             break;
-        if (x > ans)
-            ans = x;
+        if (x > *max)
+            *max = x;
+        if (x < *min)
+            *min = x;
     }
-    return ans;
+    return;
 }
